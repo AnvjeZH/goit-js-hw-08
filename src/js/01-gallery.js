3d71882ef74a594628fab86b541c897085562903
Anzhelika Zhupynas <121193138+AnvjeZH@users.noreply.github.com>
2023-05-21 18:20:06 +0300

    done task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
+// // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+// // Change code below this line
 
-console.log(galleryItems);
+const listRef = document.querySelector('.gallery');
+const markup = galleryItems.map(({description, original, preview}) => {
+return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`
+}).join('');
+
+listRef.insertAdjacentHTML('beforeend', markup);
+
+const lightBox = new SimpleLightbox('.gallery a', ({
+    captionsData: `alt`,
+    captionPosition: 'bottom',
+    captionDelay: 250
+    }));
+
+    console.log(galleryItems);
